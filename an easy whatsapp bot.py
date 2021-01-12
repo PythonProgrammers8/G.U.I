64828e091f7f31495500149a9fbcdee7e343d4ef
The Python Programmers 8 <76901729+PythonProgrammers8@users.noreply.github.com>
2021-01-12 09:52:41 +0530

    Create an easy whatsapp bot.py

---
+++ an easy whatsapp bot.py
@@ -0,0 +1,4 @@
+#for this to work on your pc you must have pywhatkit installed which can be installed in terminal woth command pip install pywhatkit
+import pywhatkit
+pywhatkit.sendmsg(”XXXXXXXXX",”hi”,10,12)
+
