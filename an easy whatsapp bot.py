d6ed9c1f248b0e909ab99200cd4cedff894d388c
The Python Programmers 8 <76901729+PythonProgrammers8@users.noreply.github.com>
2021-01-12 12:26:16 +0530

    Delete an easy whatsapp bot.py

--- an easy whatsapp bot.py
+++
@@ -1,4 +0,0 @@
-#for this to work on your pc you must have pywhatkit installed which can be installed in terminal woth command pip install pywhatkit
-import pywhatkit
-pywhatkit.sendmsg(”XXXXXXXXX",”hi”,10,12)
-
